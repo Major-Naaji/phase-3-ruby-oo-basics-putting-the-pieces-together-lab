e55203f3bccd48157e644dd4a5d7e8b7dd5b44bc
Zakaria <najkacade100@gmail.com>
2022-11-06 14:37:22 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+# Make your book class here!
+class Book
+    attr_accessor :page_count, :title, :author, :genre
+    def initialize (title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+fav_book = Book.new("The 5 Golden Rules")
+fav_book.author = "Zak"
+fav_book.genre = "Fantasy"
+fav_book.page_count = 250
+fav_book.turn_page
